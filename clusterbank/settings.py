13b0421fb0034c2128a86f6951ce8880c6499ff8
Jonathon Anderson <janderso@alcf.anl.gov>
2007-07-24 16:13:45 +0000

    Merged in changes for 0.2.1.
    git-svn-id: https://svn.mcs.anl.gov/repos/clusterbank/trunk@109 0b55062b-bd33-0410-a79d-d5943e5cb07c

--- clusterbank/settings.py
+++ clusterbank/settings.py
@@ -2,7 +2,7 @@ import os
 import ConfigParser
 
 config = ConfigParser.ConfigParser()
-config.read(["/etc/clusterbank"])
+config.read(["/etc/clusterbank.conf"])
 
 DATABASE_URI = config.get("database", "DATABASE_URI")
 UPSTREAM_DATABASE_URI = config.get("database", "UPSTREAM_DATABASE_URI")
