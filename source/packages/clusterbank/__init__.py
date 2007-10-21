c571b7032d3610a9ed4cd37355a9bd964e839fc6
Jonathon Anderson <janderso@alcf.anl.gov>
2007-10-21 01:01:05 +0000

    Moved upstream module selection to upstream module.
    git-svn-id: https://svn.mcs.anl.gov/repos/clusterbank/trunk@242 0b55062b-bd33-0410-a79d-d5943e5cb07c

--- source/packages/clusterbank/__init__.py
+++ source/packages/clusterbank/__init__.py
@@ -1,7 +1,6 @@
 import warnings
 from ConfigParser import SafeConfigParser, NoSectionError, NoOptionError
 
-import sqlalchemy
 from sqlalchemy import create_engine
 
 import clusterbank.model
@@ -11,15 +10,8 @@ __all__ = ["model", "scripting", "upstream"]
 
 __version__ = "0.2.x"
 
-def get_end_module (name):
-    mod = __import__(name)
-    components = name.split(".")
-    for comp in components[1:]:
-        mod = getattr(mod, comp)
-    return mod
-
 config = SafeConfigParser()
-config.read(["/etc/clusterbank.conf", "clusterbank.conf"])
+config.read(["/etc/clusterbank.conf"])
 
 try:
     uri = config.get("main", "database")
@@ -30,17 +22,3 @@ else:
         clusterbank.model.metadata.bind = create_engine(uri)
     except Exception, e:
         warnings.warn("invalid database: %s (%s)" % (uri, e), ImportWarning)
-
-try:
-    upstream_module_name = config.get("main", "upstream")
-except (NoSectionError, NoOptionError):
-    warnings.warn("no upstream module specified", ImportWarning)
-else:
-    try:
-        upstream_module = get_end_module(upstream_module_name)
-    except ImportError:
-        warnings.warn("invalid upstream module: %s" % (upstream_module_name), ImportWarning)
-    else:
-        clusterbank.upstream.User = upstream_module.User
-        clusterbank.upstream.Project = upstream_module.Project
-        clusterbank.upstream.Resource = upstream_module.Resource
--- source/packages/clusterbank/upstream/__init__.py
+++ source/packages/clusterbank/upstream/__init__.py
@@ -43,4 +43,32 @@ class Resource:
 class DoesNotExist (Exception)
 """
 
+import warnings
+from ConfigParser import SafeConfigParser, NoSectionError, NoOptionError
+
 __all__ = ["userbase"]
+
+config = SafeConfigParser()
+config.read(["/etc/clusterbank.conf"])
+
+def get_end_module (name):
+    mod = __import__(name)
+    components = name.split(".")
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+try:
+    upstream_module_name = config.get("main", "upstream")
+except (NoSectionError, NoOptionError):
+    warnings.warn("no upstream module specified", ImportWarning)
+else:
+    try:
+        upstream_module = get_end_module(upstream_module_name)
+    except (ImportError, AttributeError):
+        warnings.warn("invalid upstream module: %s" % (upstream_module_name), ImportWarning)
+    else:
+        User = upstream_module.User
+        Project = upstream_module.Project
+        Resource = upstream_module.Resource
+        __all__.extend(["User", "Project", "Resource"])
