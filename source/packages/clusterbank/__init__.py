edebaf1c6fe0ff50a0b4a339b0fe77b2d6e77fb2
Jonathon Anderson <janderso@alcf.anl.gov>
2008-04-23 21:53:01 +0000

    Use default upstream module by default.
    git-svn-id: https://svn.mcs.anl.gov/repos/clusterbank/trunk@437 0b55062b-bd33-0410-a79d-d5943e5cb07c

--- source/packages/clusterbank/__init__.py
+++ source/packages/clusterbank/__init__.py
@@ -14,17 +14,15 @@ __all__ = ["model", "interfaces", "upstream"]
 
 __version__ = "trunk"
 
-upstream = None
-
 config = SafeConfigParser()
 config.read(["/etc/clusterbank.conf"])
 
 try:
     upstream_module_name = config.get("upstream", "module")
 except (NoSectionError, NoOptionError):
-    warnings.warn("no upstream module specified", UserWarning)
-else:
-    try:
-        upstream = __import__(upstream_module_name, locals(), globals(), ["get_project_name", "get_project_id", "get_resource_name", "get_resource_id"])
-    except ImportError:
-        warnings.warn("invalid upstream module: %s" % (upstream_module_name), UserWarning)
+    upstream_module_name = "clusterbank.upstream.default"
+try:
+    upstream = __import__(upstream_module_name, locals(), globals(), ["get_project_name", "get_project_id", "get_resource_name", "get_resource_id"])
+except ImportError:
+    warnings.warn("invalid upstream module: %s" % (upstream_module_name), UserWarning)
+    import clusterbank.upstream.default as upstream
