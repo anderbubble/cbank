f4040bf131aae2cd20835db17b02c0e0413ab762
Jonathon Anderson <janderso@alcf.anl.gov>
2008-07-02 18:39:37 +0000

    Testsuite fixture now modifies configuration in-place.
    git-svn-id: https://svn.mcs.anl.gov/repos/clusterbank/trunk@493 0b55062b-bd33-0410-a79d-d5943e5cb07c

--- testsuite/test_clusterbank/__init__.py
+++ testsuite/test_clusterbank/__init__.py
@@ -1,7 +1,15 @@
 import ConfigParser
 
-import clusterbank.model
+import clusterbank
 
 __all__ = ["test_model", "test_cbank", "test_upstreams"]
 
-clusterbank.model.entities.config = ConfigParser.SafeConfigParser()
+_config = None
+
+def setup ():
+    for section in clusterbank.config.sections():
+        clusterbank.config.remove_section(section)
+    if clusterbank.config.has_section("DEFAULT"):
+        for option in clusterbank.config.options("DEFAULT"):
+            clusterbank.config.remove_option("DEFAULT", option)
+
