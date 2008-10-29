c8dec0c758302bf9bec27922728f9e7288cbedbf
Jonathon Anderson <janderso@alcf.anl.gov>
2008-10-29 22:01:10 +0000

    Cleaned up the default upstream module. (pylint)
    git-svn-id: https://svn.mcs.anl.gov/repos/clusterbank/trunk@732 0b55062b-bd33-0410-a79d-d5943e5cb07c

--- source/packages/clusterbank/upstreams/default.py
+++ source/packages/clusterbank/upstreams/default.py
@@ -6,6 +6,7 @@ Resource -- upstream resource
 User -- upstream user
 """
 
+
 __all__ = [
     "get_project_id", "get_project_name",
     "get_project_members", "get_project_owners",
@@ -14,87 +15,132 @@ __all__ = [
     "get_member_projects", "get_owner_projects",
 ]
 
+
 class Entity (object):
     
-    def __init__ (self, id, name):
-        self.id = id
+    """Generic entities.
+    
+    Attributes:
+    id -- the entity id
+    name -- the entity name
+    """
+    
+    def __init__ (self, id_, name):
+        """Initialize a new entity.
+        
+        Arguments:
+        id -- the entity id
+        name -- the entity name
+        """
+        self.id = id_
         self.name = name
 
 
 class User (Entity):
-    pass
+    
+    """User entities."""
 
 
 class Project (Entity):
     
-    def __init__ (self, id, name):
-        Entity.__init__(self, id, name)
+    """Project entities."""
+    
+    def __init__ (self, id_, name):
+        """Initialize a new project.
+        
+        Arguments:
+        id -- the project id
+        name -- the project name
+        """
+        Entity.__init__(self, id_, name)
         self.members = []
         self.owners = []
 
 
 class Resource (Entity):
-    pass
+    
+    """Resource entities."""
 
-users = []
-projects = []
-resources = []
-
-def get_project_id (name):
-    for project in projects:
-        if project.name == name:
-            return project.id
-    return None
-
-def get_project_name (id):
-    for project in projects:
-        if project.id == id:
-            return project.name
-    return None
-
-def get_project_members (id):
-    for project in projects:
-        if project.id == id:
-            return [user.id for user in project.members]
-    return []
-
-def get_project_owners (id):
-    for project in projects:
-        if project.id == id:
-            return [user.id for user in project.owners]
-    return []
-
-def get_member_projects (id):
-    return [project.id for project in projects
-        if [user for user in project.members
-            if user.id == id]]
-
-def get_owner_projects (id):
-    return [project.id for project in projects
-        if [user for user in project.owners
-            if user.id == id]]
-
-def get_resource_id (name):
-    for resource in resources:
-        if resource.name == name:
-            return resource.id
-    return None
-
-def get_resource_name (id):
-    for resource in resources:
-        if resource.id == id:
-            return resource.name
-    return None
 
 def get_user_id (name):
+    """Given a user name, return the user id."""
     for user in users:
         if user.name == name:
             return user.id
     return None
 
-def get_user_name (id):
+
+def get_user_name (id_):
+    """Given a user id, return the user name."""
     for user in users:
-        if user.id == id:
+        if user.id == id_:
             return user.name
     return None
+
+
+def get_project_id (name):
+    """Given a project name, return the project id, or None."""
+    for project in projects:
+        if project.name == name:
+            return project.id
+    return None
+
+
+def get_project_name (id_):
+    """Given a project id, return the project name or None."""
+    for project in projects:
+        if project.id == id_:
+            return project.name
+    return None
+
+
+def get_resource_id (name):
+    """Given a resource name, return the resource id."""
+    for resource in resources:
+        if resource.name == name:
+            return resource.id
+    return None
+
+
+def get_resource_name (id_):
+    """Given a resource id, return the resource name."""
+    for resource in resources:
+        if resource.id == id_:
+            return resource.name
+    return None
+
+
+def get_project_members (id_):
+    """Given a project id, return the ids of the project's members."""
+    for project in projects:
+        if project.id == id_:
+            return [user.id for user in project.members]
+    return []
+
+
+def get_project_owners (id_):
+    """Given a project id, return the ids of the project's owners."""
+    for project in projects:
+        if project.id == id_:
+            return [user.id for user in project.owners]
+    return []
+
+
+def get_member_projects (id_):
+    """Given a user id, return the ids of the user's projects."""
+    return [project.id for project in projects
+        if [user for user in project.members
+            if user.id == id_]]
+
+
+def get_owner_projects (id_):
+    """Given a user id, return the ids of the user's owned projects."""
+    return [project.id for project in projects
+        if [user for user in project.owners
+            if user.id == id_]]
+
+
+users = []
+projects = []
+resources = []
 
