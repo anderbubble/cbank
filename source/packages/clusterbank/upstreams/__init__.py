7b899e33f42a806410a9972d70d8355e6b7c9221
Jonathon Anderson <janderso@alcf.anl.gov>
2008-10-30 17:06:35 +0000

    Added a docstring to clusterbank.upstreams (pylint)
    git-svn-id: https://svn.mcs.anl.gov/repos/clusterbank/trunk@733 0b55062b-bd33-0410-a79d-d5943e5cb07c

--- source/packages/clusterbank/upstreams/__init__.py
+++ source/packages/clusterbank/upstreams/__init__.py
@@ -1,2 +1,8 @@
+"""Official upstream modules.
+
+default -- A memory-only upstream module used for testing. (default)
+posix -- An upstream module based on /etc/passwd and /etc/group
+"""
+
 __all__ = ["default", "posix"]
 
