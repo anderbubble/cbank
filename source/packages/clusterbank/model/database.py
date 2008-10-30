66c768cffc441598828e1cef7569d9828dea319c
Jonathon Anderson <janderso@alcf.anl.gov>
2008-10-30 20:10:09 +0000

    Cleaned up imports in clusterbank.model.database (pylint)
    git-svn-id: https://svn.mcs.anl.gov/repos/clusterbank/trunk@739 0b55062b-bd33-0410-a79d-d5943e5cb07c

--- source/packages/clusterbank/model/database.py
+++ source/packages/clusterbank/model/database.py
@@ -17,7 +17,8 @@ refunds -- refunds
 from datetime import datetime
 
 from sqlalchemy import MetaData, Table, Column, ForeignKey, \
-    ForeignKeyConstraint, UniqueConstraint, types
+    ForeignKeyConstraint
+from sqlalchemy.types import Integer, DateTime, Text, Boolean
 
 __all__ = [
     "metadata",
@@ -28,67 +29,67 @@ __all__ = [
 metadata = MetaData()
 
 users = Table("users", metadata,
-    Column("id", types.Integer, primary_key=True),
+    Column("id", Integer, primary_key=True),
     mysql_engine="InnoDB")
 
 projects = Table("projects", metadata,
-    Column("id", types.Integer, primary_key=True),
+    Column("id", Integer, primary_key=True),
     mysql_engine="InnoDB")
 
 resources = Table("resources", metadata,
-    Column("id", types.Integer, primary_key=True),
+    Column("id", Integer, primary_key=True),
     mysql_engine="InnoDB")
 
 allocations = Table("allocations", metadata,
-    Column("id", types.Integer, primary_key=True),
+    Column("id", Integer, primary_key=True),
     Column("project_id", None, ForeignKey("projects.id"), nullable=False),
     Column("resource_id", None, ForeignKey("resources.id"), nullable=False),
-    Column("datetime", types.DateTime, nullable=False, default=datetime.now),
-    Column("amount", types.Integer, nullable=False),
-    Column("start", types.DateTime, nullable=False),
-    Column("expiration", types.DateTime, nullable=False),
-    Column("comment", types.Text),
+    Column("datetime", DateTime, nullable=False, default=datetime.now),
+    Column("amount", Integer, nullable=False),
+    Column("start", DateTime, nullable=False),
+    Column("expiration", DateTime, nullable=False),
+    Column("comment", Text),
     mysql_engine="InnoDB")
 
 holds = Table("holds", metadata,
-    Column("id", types.Integer, primary_key=True),
+    Column("id", Integer, primary_key=True),
     Column("allocation_id", None, ForeignKey("allocations.id"), nullable=False),
-    Column("datetime", types.DateTime, nullable=False, default=datetime.now),
-    Column("amount", types.Integer, nullable=False),
+    Column("datetime", DateTime, nullable=False, default=datetime.now),
+    Column("amount", Integer, nullable=False),
     Column("user_id", None, ForeignKey("users.id"), nullable=True),
-    Column("comment", types.Text),
-    Column("active", types.Boolean, nullable=False, default=True),
+    Column("comment", Text),
+    Column("active", Boolean, nullable=False, default=True),
     mysql_engine="InnoDB")
 
 jobs = Table("jobs", metadata,
     Column("resource_id", None, ForeignKey("resources.id"), primary_key=True),
-    Column("id", types.Integer, primary_key=True, autoincrement=False),
-    Column("start", types.DateTime),
-    Column("end", types.DateTime),
+    Column("id", Integer, primary_key=True, autoincrement=False),
+    Column("start", DateTime),
+    Column("end", DateTime),
     mysql_engine="InnoDB")
 
 charges = Table("charges", metadata,
-    Column("id", types.Integer, primary_key=True),
+    Column("id", Integer, primary_key=True),
     Column("allocation_id", None, ForeignKey("allocations.id"), nullable=False),
-    Column("datetime", types.DateTime, nullable=False, default=datetime.now),
-    Column("amount", types.Integer, nullable=False),
+    Column("datetime", DateTime, nullable=False, default=datetime.now),
+    Column("amount", Integer, nullable=False),
     Column("user_id", None, ForeignKey("users.id"), nullable=True),
-    Column("comment", types.Text),
+    Column("comment", Text),
     mysql_engine="InnoDB")
 
 jobs_charges = Table("jobs_charges", metadata,
-    Column("job_resource_id", types.Integer),
-    Column("job_id", types.Integer),
+    Column("job_resource_id", Integer),
+    Column("job_id", Integer),
     Column("charge_id", None, ForeignKey("charges.id")),
     ForeignKeyConstraint(["job_resource_id", "job_id"],
         ["jobs.resource_id", "jobs.id"]),
     mysql_engine="InnoDB")
 
 refunds = Table("refunds", metadata,
-    Column("id", types.Integer, primary_key=True),
+    Column("id", Integer, primary_key=True),
     Column("charge_id", None, ForeignKey("charges.id"), nullable=False),
-    Column("datetime", types.DateTime, nullable=False, default=datetime.now),
-    Column("amount", types.Integer, nullable=False),
-    Column("comment", types.Text),
+    Column("datetime", DateTime, nullable=False, default=datetime.now),
+    Column("amount", Integer, nullable=False),
+    Column("comment", Text),
     mysql_engine="InnoDB")
 
