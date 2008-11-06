43aaceb02c8935f80c13db908f987260406be7c7
Jonathon Anderson <janderso@alcf.anl.gov>
2008-11-06 18:07:04 +0000

    Added convenient access to model, controllers, and exceptions.
    git-svn-id: https://svn.mcs.anl.gov/repos/clusterbank/trunk@760 0b55062b-bd33-0410-a79d-d5943e5cb07c

--- source/packages/clusterbank/__init__.py
+++ source/packages/clusterbank/__init__.py
@@ -5,11 +5,23 @@ A library for managing and accounting for the use of resources.
 
 import ConfigParser
 
-__all__ = ["config", "__version__"
-    "exceptions", "model", "controllers", "cbank", "upstreams"]
+__all__ = ["config", "__version__", "exceptions", "model", "controllers",
+    "cbank", "upstreams"]
 
 __version__ = "trunk"
 
 config = ConfigParser.SafeConfigParser()
 config.read(["/etc/clusterbank.conf"])
+
+# import common elements for convenience
+from clusterbank.model import *
+from clusterbank.controllers import *
+from clusterbank.exceptions import *
+
+# update __all__
+import clusterbank.model
+import clusterbank.controllers
+import clusterbank.exceptions
+__all__ = (__all__ + clusterbank.model.__all__ +
+    clusterbank.controllers.__all__ + clusterbank.exceptions.__all__)
 
