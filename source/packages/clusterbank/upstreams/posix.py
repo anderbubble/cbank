7330a9308f46a9fe735f249c8ec02ff09aa480fb
Jonathon Anderson <anderbubble@gmail.com>
2010-05-31 21:32:06 +0300

    Updated posix upstream module for new upstream api.

--- source/packages/clusterbank/upstreams/posix.py
+++ source/packages/clusterbank/upstreams/posix.py
@@ -4,71 +4,93 @@ from pwd import getpwnam, getpwuid, getpwall
 from grp import getgrnam, getgrgid, getgrall
 
 __all__ = [
-    "get_project_id", "get_project_name",
-    "get_project_members", "get_project_admins",
-    "get_resource_id", "get_resource_name",
-    "get_user_id", "get_user_name",
-    "get_member_projects", "get_admin_projects",
+    "user_in", "user_out",
+    "project_in", "project_out",
+    "resource_in", "resource_out",
+    "project_member", "project_manager",
 ]
 
-def get_project_id (name):
-    """Given a group name, return the gid, or None."""
+
+def user_in (user_string):
     try:
-        return getgrnam(name)[2]
-    except KeyError:
-        return None
+        uid = int(user_string)
+    except ValueError:
+        try:
+            return getpwnam(user_string)[2]
+        except KeyError:
+            return str(user_string)
+    else:
+        return getpwuid(uid)[2]
 
-def get_project_name (gid):
-    """Given a gid, return the group name, or None."""
+
+def user_out (id_):
     try:
-        return getgrgid(int(gid))[0]
-    except KeyError:
-        return None
+        uid = int(id_)
+    except ValueError:
+        return str(id_)
+    else:
+        try:
+            return getpwuid(uid)[0]
+        except KeyError:
+            return str(id_)
 
-def get_project_members (gid):
-    """Given a gid, return the group member uids."""
+
+def group_in (group_string):
     try:
-        return [str(getpwnam(name)[2]) for name in getgrgid(int(gid))[3]]
-    except KeyError:
-        return []
+        gid = int(group_string)
+    except ValueError:
+        try:
+            return getgrnam(group_string)[2]
+        except KeyError:
+            return str(group_string)
+    else:
+        return getgrgid(gid)[2]
 
-def get_project_admins (gid):
-    """Given a gid, return the uids of users with that default group."""
+
+def group_out (id_):
     try:
-        return [user[2] for user in getpwall() if user[3] == getgrgid(int(gid))[0]]
-    except KeyError:
-        return []
+        gid = int(id_)
+    except ValueError:
+        return str(id_)
+    else:
+        try:
+            return getgrgid(gid)[0]
+        except KeyError:
+            return str(id_)
 
-def get_member_projects (uid):
-    """Given a uid, return the gids of that user's projects."""
+
+resource_in = project_in = group_in
+resource_out = project_out = group_out
+
+
+def project_member (project_id, user_id):
+    if project_manager(project_id, user_id):
+        return True
     try:
-        return [str(group[2]) for group in getgrall()
-            if getpwuid(int(uid))[0] in group[3]]
-    except KeyError:
-        return []
-
-def get_admin_projects (uid):
-    """Given a uid, return a list containing that user's default group."""
-    return [str(getgrgid(getpwuid(int(uid))[3])[2])]
-
-def get_resource_id (name):
-    """Given a group name, return the gid, or None."""
-    return get_project_id(name)
-
-def get_resource_name (gid):
-    """Given a gid, return the group name, or None."""
-    return get_project_name(gid)
-
-def get_user_id (username):
-    """Given a username, return the uid, or None."""
+        gid = int(project_id)
+        uid = int(user_id)
+    except ValueError:
+        return False
     try:
-        return getpwnam(username)[2]
+        users = getgrgid(gid)[3]
     except KeyError:
-        return None
+        return False
 
-def get_user_name (uid):
-    """Given a uid, return the username, or None."""
+    for user_string in getgrgid(gid)[3]:
+        if uid == user_in(user_string):
+            return True
+    return False
+
+def project_manager (project_id, user_id):
     try:
-        return getpwuid(int(uid))[0]
+        gid = int(project_id)
+        uid = int(user_id)
+    except ValueError:
+        return False
+    try:
+        user_gid = getpwuid(uid)[3]
     except KeyError:
-        return None
+        return False
+
+    if user_gid == gid:
+        return True
